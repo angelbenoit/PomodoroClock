962fb8acfdf98ad8c7146a6187a2fe00a3e82e59
Angel Contreras <angelcbenoit@gmail.com>
2017-12-25 23:40:29 -0800

    changed the seconds display

--- clock.js
+++ clock.js
@@ -1,13 +1,19 @@
 var counter = 0;
 var minute = 5;
 var countdown;
-$("#time").text(minute + ":" + counter);
+$("#time").text(minute + ":" + "0" + counter);
 
 $("#start").click(function(){
     countdown = setInterval(function(){
         counter--;
         if(counter > 0 && minute >= 0){
-            $("#time").text(minute + ":" + counter);
+            if(counter < 10){
+                $("#time").text(minute + ":" + "0" + counter);
+            }
+            else{
+                $("#time").text(minute + ":" + counter);
+            }
+
         }
         else{
             counter = 60;
@@ -27,23 +33,34 @@ $("#pause").click(function(){
 $("#reset").click(function(){
     counter = 0;
     minute = 5;
-    $("#time").text(minute + ":" + counter);
+    $("#time").text(minute + ":" + "0" + counter);
     $("#minus").prop("disabled", false);
 });
 
 $("#add").click(function () {
     minute++;
-    $("#time").text(minute + ":" + counter);
+    if(counter < 10){
+        $("#time").text(minute + ":" + "0" + counter);
+    }
+    else{
+        $("#time").text(minute + ":" + counter);
+    }
     $("#minus").prop("disabled", false);
 });
 
 $("#minus").click(function () {
+
     if(minute === 1){
         $(this).attr("disabled", true);
     }
     else if(minute > 1){
         minute--;
-        $("#time").text(minute + ":" + counter);
+        if(counter < 10){
+            $("#time").text(minute + ":" + "0" + counter);
+        }
+        else{
+            $("#time").text(minute + ":" + counter);
+        }
     }
 
 });
